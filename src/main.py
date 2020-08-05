525820c4bb54cc8f1c479ac496d29dc19c13a214
Tim <tim@TimDesktop01-popos.localdomain>
2020-08-05 13:31:14 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@ queries["textOfPapersAndPatents"] = 'CALL db.index.fulltext.createNodeIndex("tex
 queries["GeneSymbolFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("GeneSymbolFullTextIndex",["GeneSymbol"],["sid"])'
 queries["AuthorFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("AuthorFullTextIndex",["Author"],["first", "middle","last"])'
 queries["EntityFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("EntityFullTextIndex",["Entity"],["name"])'
-g = Graph(os.getenv("GC_NEO4J_URL", None), user=os.getenv(
-    "GC_NEO4J_USER", None), password=os.getenv("GC_NEO4J_PASSWORD", None))
+neo4j_host = os.getenv("GC_NEO4J_URL", None)
+neo4j_pw = os.getenv("GC_NEO4J_PASSWORD", None)
+neo4j_user = os.getenv("GC_NEO4J_USER", None)
+print("Connect to '{}'@'{}'".format(neo4j_user, neo4j_host))
+g = Graph(neo4j_host, user=neo4j_user, password=neo4j_pw)
 
 for name, query in queries.items():
     print("Create fulltext index '{}'".format(name))
