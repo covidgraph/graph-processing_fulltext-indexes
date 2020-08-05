9e88c944aba3fe8a36077bdc3c1c67cb42a51402
Tim <tim@TimDesktop01-popos.localdomain>
2020-08-05 12:11:59 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
+import os
+from py2neo import Graph
 queries = {}
 
 queries["textOfPapersAndPatents"] = 'CALL db.index.fulltext.createNodeIndex("textOfPapersAndPatents",["Fragment", "Abstract", "Paper", "Patent", "PatentTitle", "PatentClaim","PatentAbstract"],["title", "text"])'
 queries["GeneSymbolFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("GeneSymbolFullTextIndex",["GeneSymbol"],["sid"])'
 queries["AuthorFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("AuthorFullTextIndex",["Author"],["first", "middle","last"])'
 queries["EntityFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("EntityFullTextIndex",["Entity"],["name"])'
-g = Graph()
-
-log = None
+g = Graph(host=os.getenv("GC_NEO4J_URL", None), user=os.getenv(
+    "GC_NEO4J_USER", None), password=os.getenv("GC_NEO4J_PASSWORD", None))
 
 for name, query in queries.items():
-    log.info("Create fulltext index '{}'".format(name))
+    print("Create fulltext index '{}'".format(name))
     g.run(query)
