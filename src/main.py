6afb9d933625d09e948ed49c883d2646e9f9f668
Tim <tim@TimDesktop01-popos.localdomain>
2020-08-05 13:09:12 +0200

    catch allready existing indexes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import os
 from py2neo import Graph
+from py2neo.database import ClientError
 queries = {}
 
 queries["textOfPapersAndPatents"] = 'CALL db.index.fulltext.createNodeIndex("textOfPapersAndPatents",["Fragment", "Abstract", "Paper", "Patent", "PatentTitle", "PatentClaim","PatentAbstract"],["title", "text"])'
 queries["GeneSymbolFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("GeneSymbolFullTextIndex",["GeneSymbol"],["sid"])'
 queries["AuthorFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("AuthorFullTextIndex",["Author"],["first", "middle","last"])'
 queries["EntityFullTextIndex"] = 'CALL db.index.fulltext.createNodeIndex("EntityFullTextIndex",["Entity"],["name"])'
-g = Graph(host=os.getenv("GC_NEO4J_URL", None), user=os.getenv(
+g = Graph(os.getenv("GC_NEO4J_URL", None), user=os.getenv(
     "GC_NEO4J_USER", None), password=os.getenv("GC_NEO4J_PASSWORD", None))
 
 for name, query in queries.items():
     print("Create fulltext index '{}'".format(name))
-    g.run(query)
+    try:
+        g.run(query)
+    except ClientError as e:
+        print("Index '{}' allready exists. Skip.".format(name))
